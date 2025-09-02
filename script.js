5bce3c8ab5fdb61b8dcea57e5f5834af39925974
m3adbigboss200-ui <m3adbigboss200@gmail.com>
2025-09-02 19:08:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,4 +14,8 @@ function openTikTok() {
 function openInstagram() {
   window.open("https://www.instagram.com/yemen777173?igsh=MTY1dTN3Y3Z5Mng4aA==");
 }
+function sendMessage() {
+  const message = document.getElementById('messageInput').value;
+  alert("الرسالة التي كتبتها هي: " + message);
+}
 
