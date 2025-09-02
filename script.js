450d9063a7163186d1aa40a94afdcab3c7d4f6d6
m3adbigboss200-ui <m3adbigboss200@gmail.com>
2025-09-02 17:29:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 function showMessage() {
   alert("لقد قمت بالضغط على الزر!");
 }
+function showMessage() {
+  alert("لقد قمت بالضغط على الزر!");
+}
+
+function openYouTube() {
+  window.open("https://youtube.com/@2_c_7_89?si=sESzRSJQ6dEsZK9P");
+}
+
+function openX() {
+  window.open("https://x.com/2_c_7_89?t=bIC4BE2Sq0_qM1R1ZVOjQg&s=09");
+}
+
+function openTikTok() {
+  window.open("https://www.tiktok.com/@2_c_7_89?_t=ZS-8zOQuHVrLpo&_r=1");
+}
+
+function openInstagram() {
+  window.open("https://www.instagram.com/yemen777173?igsh=MTY1dTN3Y3Z5Mng4aA==");
+}
