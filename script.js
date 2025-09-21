ed5e91796ae10005e8b171c42026cad9d3675f9e
m3adbigboss200-ui <m3adbigboss200@gmail.com>
2025-09-21 18:12:43 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-// روابط مواقع التواصل الاجتماعي
-function openYouTube() {
-  window.open("https://youtube.com/@2_c_7_89?si=sESzRSJQ6dEsZK9P");
-}
-
-function openYouTubeBackup() {
-    window.open("https://youtube.com/@2_7_8_ye?si=2fuMXjZsl7o7piBT");
-}
-
-function openX() {
-  window.open("https://x.com/2_c_7_89?t=bIC4BE2Sq0_qM1R1ZVOjQg&s=09");
-}
-
-function openTikTok() {
-  window.open("https://www.tiktok.com/@2_c_7_89?_t=ZS-8zOQuHVrLpo&_r=1");
-}
-
-function openInstagram() {
-  window.open("https://www.instagram.com/yemen777173?igsh=MTY1dTN3Y3Z5Mng4aA==");
-}
-
-function openTelegram() {
-  window.open("https://t.me/d2_c_7_89");
-}
-
 // دالة عرض الوقت والتاريخ الهجري والميلادي
 function updateDateTime() {
     const dateTimeElement = document.getElementById('date-time');
